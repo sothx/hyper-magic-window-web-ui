1769023a739f74c045ef4f1fd5fc0b30d8564926
sothx <i@sothx.com>
2024-10-11 19:47:24 +0800

    feat: update

--- src/types/SettingRuleItem.d.ts
+++ src/types/SettingRuleItem.d.ts
@@ -8,21 +8,21 @@ export default interface SettingRuleItem {
     /** 是否启用嵌入式规则，默认 false（可选） */
     embeddedEnable?: boolean;
     
-    /** 是否启用固定方向规则，默认 false（可选） */
+    /** 是否启用固定方向规则（Hyper OS 2.0+ 有效），默认 false（可选） */
     fixedOrientationEnable?: boolean;
     
-    /** 是否启用全屏规则，默认 false（可选） */
+    /** 是否启用全屏规则（Hyper OS 2.0+ 有效），默认 false（可选） */
     fullScreenEnable?: boolean;
     
-    /** 配置是否已被修改，默认 false（可选） */
+    /** 配置是否已被修改（Hyper OS 2.0+ 有效），默认 false（可选） */
     isModified?: boolean;
     
-    /** 是否启用 16:9 比例，默认 false（可选） */
+    /** 是否启用 16:9 比例（Hyper OS 2.0+ 有效），默认 false（可选） */
     ratio_16_9_Enable?: boolean;
     
-    /** 是否启用 4:3 比例，默认 false（可选） */
+    /** 是否启用 4:3 比例（Hyper OS 2.0+ 有效），默认 false（可选） */
     ratio_4_3_Enable?: boolean;
     
-    /** 是否启用全屏比例，默认 false（可选） */
+    /** 是否启用全屏比例（Hyper OS 2.0+ 有效），默认 false（可选） */
     ratio_fullScreenEnable?: boolean;
 }
