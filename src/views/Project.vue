f9e89cb973c013efe95ce6cd91f00c57271028ec
sothx <i@sothx.com>
2024-10-24 07:19:48 +0800

    feat: update

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -27,8 +27,8 @@ configProviderProps: configProviderPropsRef
           （写不过来QwQ，可能进展缓慢，请勿催更~）
         </p>
       </div>
-      <div class="mt-6 border-t border-gray-100">
-        <dl class="divide-y divide-gray-100 mb-5">
+      <div class="mt-6 border-gray-100">
+        <dl class="divide-gray-100 mb-5">
           <!-- <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
             <dt class="text-sm font-medium leading-6 text-gray-900">模块版本号</dt>
             <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">暂定</dd>
