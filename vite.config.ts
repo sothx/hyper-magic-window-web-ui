0ebf3705d33293d1bfe11b4e2fcc80801c7eb4dc
SharpIce <VupRbl@163.com>
2024-10-23 20:08:27 +0800

    refactor: 清理项目配置文件
    - 将 Prettier 配置合并到 package.json 中
    - 将 Tailwindcss 配置文件从 JavaScript 改为 TypeScript
    - 将 Postcss 配置文件合并到 Vite 配置文件内
    - 移除 Vite 配置文件无用项
    - 移除无用包
    - package.json 添加 devEngines 和 browserslist 项
    
    Signed-off-by: SharpIce <VupRbl@163.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,62 +1,74 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { parse } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import { exec } from 'child_process';
-import { ServerResponse } from 'http';
-import { parse } from 'url';
-import legacy from '@vitejs/plugin-legacy'
+import tailwindcss from 'tailwindcss';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import { fileURLToPath, URL } from 'node:url';
+import postcssPresetEnv from 'postcss-preset-env';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-    {
-      name: "run-powershell-command",
-      configureServer(server) {
-        server.middlewares.use('/api/exec', (req, res) => {
-          if (process.env.NODE_ENV === 'development') {
-            const queryObject = parse(req.url as string, true).query;
-            const command = queryObject.cmd as string  // 默认命令
-            const shell = process.platform === 'win32' ? 'powershell.exe' : '/bin/bash';
-            if (command) {
-              exec(command,{
-                shell
-              }, (error, stdout, stderr) => {
-                if (error) {
-                  res.statusCode = 500;
-                  res.end(`Error: ${error.message}`);
-                  return;
-                }
-                if (stderr) {
-                  res.statusCode = 500;
-                  res.end(`Error: ${stderr}`);
-                  return;
-                }
-                res.statusCode = 200;
-                res.end(stdout);  // 返回命令执行结果
-              });
-            } else {
-              res.statusCode = 500;
-              res.end(`Error: unknow command`);
-            }
-          }
-        })
-      }
-    },
-    // legacy({
-    //   targets: ['defaults', 'not IE 11'],
-    // }),
-  ],
-  
-  // assetsInclude: ['**/*.xml'], // 添加这一行以包括 XML 文件
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+	css: {
+		postcss: {
+			plugins: [
+				tailwindcss(),
+				postcssPresetEnv({
+					stage: 4,
+				}),
+			],
+		},
+	},
+	plugins: [
+		vue(),
+		vueJsx(),
+		vueDevTools(),
+		{
+			name: 'run-powershell-command',
+			configureServer(server) {
+				server.middlewares.use('/api/exec', (req, res) => {
+					if (process.env.NODE_ENV === 'development') {
+						const queryObject = parse(req.url as string, true).query;
+						const command = queryObject.cmd as string; // 默认命令
+						const shell = process.platform === 'win32' ? 'powershell.exe' : '/bin/bash';
+						if (command) {
+							exec(
+								command,
+								{
+									shell,
+								},
+								(error, stdout, stderr) => {
+									if (error) {
+										res.statusCode = 500;
+										res.end(`Error: ${error.message}`);
+										return;
+									}
+									if (stderr) {
+										res.statusCode = 500;
+										res.end(`Error: ${stderr}`);
+										return;
+									}
+									res.statusCode = 200;
+									res.end(stdout); // 返回命令执行结果
+								},
+							);
+						} else {
+							res.statusCode = 500;
+							res.end(`Error: unknow command`);
+						}
+					}
+				});
+			},
+		},
+		// legacy({
+		//   targets: ['defaults', 'not IE 11'],
+		// }),
+	],
+
+	// assetsInclude: ['**/*.xml'], // 添加这一行以包括 XML 文件
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+});
