6e97a93570c4a0d82e4b8e467d0f238b4b6e4cdf
sothx <i@sothx.com>
2024-12-11 01:30:29 +0800

    feat: update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,4 +2,14 @@ import type { Config } from 'tailwindcss';
 
 export default {
 	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	theme: {
+		extend: {},
+		screens: {
+		  'sm': '640px',
+		  'md': '768px',
+		  'lg': '896px', // 修改默认 lg 断点
+		  'xl': '1280px',
+		  '2xl': '1536px',
+		},
+	}
 } satisfies Config;
