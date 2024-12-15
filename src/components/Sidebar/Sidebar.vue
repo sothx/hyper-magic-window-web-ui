833cfee38c2ed354652f583d3b8e3f5505102ac0
sothx <i@sothx.com>
2024-12-15 17:58:08 +0800

    feat: update blur

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -484,10 +484,10 @@ onBeforeUnmount(() => {
 		</div>
 
 		<div class="min-h-screen lg:pl-72">
-			<div class="sticky top-0 z-40 lg:mx-auto lg:max-w-7xl lg:px-8">
+			<div class="sticky top-0 z-40 lg:mx-auto backdrop-blur-md">
 				<div
-					class="flex h-16 items-center gap-x-4 border-b"
-					:class="deviceStore.isDarkMode ? 'border-gray-700 bg-zinc-900' : 'border-gray-200 bg-white'">
+					class="flex h-14 items-center gap-x-4 border-b"
+					:class="deviceStore.isDarkMode ? 'border-gray-700 bg-zinc-900/50' : 'border-gray-200 bg-white/50'">
 					<button type="button" class="m-2.5 p-2.5 text-gray-700 lg:hidden" @click="sidebarOpen = true">
 						<span class="sr-only">Open sidebar</span>
 						<Bars3Icon class="h-6 w-6" aria-hidden="true" />
