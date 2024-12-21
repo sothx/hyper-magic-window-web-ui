0a604b69427db2f44382f1992198bea8756132af
sothx <i@sothx.com>
2024-12-22 00:36:47 +0800

    feat: update splash

--- src/components/SplashScreen.vue
+++ src/components/SplashScreen.vue
@@ -18,7 +18,7 @@ const deviceStore = useDeviceStore();
         left: 0;
         right: 0;
         bottom: 0;
-        background-color: rgba(255,255,255,0.5);
+        background-color: rgba(255,255,255,0.6);
         display: flex;
         justify-content: center;
         align-items: center;
