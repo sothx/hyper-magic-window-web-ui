20437aaddbd959b7796c0088be25f0fc6e1112c2
sothx <i@sothx.com>
2025-03-04 13:55:16 +0800

    feat: update

--- src/components/SplashScreen.vue
+++ src/components/SplashScreen.vue
@@ -5,8 +5,8 @@ const deviceStore = useDeviceStore();
 <template>
   <div class="splash-screen">
     <div class="flex h-full flex-col items-center justify-center">
-        <div class="mt-[61px] bg-gradient-to-t from-[#242424] to-[#262626] bg-clip-text text-center text-[40px] font-[600] leading-[53px] text-black/50 mix-blend-color-burn tablet:mt-[75px] tablet:text-[64px] tablet:leading-[85px]">完美横屏应用计划</div>
-        <div class="mt-12 bg-gradient-to-t from-[#242424] to-[#262626] bg-clip-text text-center text-[20px] font-[600] leading-[27px] text-black/50 mix-blend-color-burn tablet:mt-[23px] tablet:text-[24px] tablet:font-[600] tablet:leading-[32px]">正在加载中，请稍等……</div>
+        <div class="mt-[61px] bg-gradient-to-t from-[#242424] to-[#262626] bg-clip-text text-center text-[40px] font-[600] leading-[53px] text-black/10 mix-blend-color-burn tablet:mt-[75px] tablet:text-[64px] tablet:leading-[85px]">完美横屏应用计划</div>
+        <div class="mt-12 bg-gradient-to-t from-[#242424] to-[#262626] bg-clip-text text-center text-[20px] font-[600] leading-[27px] text-black/10 mix-blend-color-burn tablet:mt-[23px] tablet:text-[24px] tablet:font-[600] tablet:leading-[32px]">正在加载中，请稍等……</div>
     </div>
   </div>
   </template>
@@ -18,7 +18,7 @@ const deviceStore = useDeviceStore();
         left: 0;
         right: 0;
         bottom: 0;
-        background-color: rgba(255,255,255,0.5);
+        background-color: rgba(255,255,255,0.3);
         display: flex;
         justify-content: center;
         align-items: center;
