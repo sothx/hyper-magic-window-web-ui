4a81edaecd3e88f4b12b11a8aff9a395eda07c46
sothx <i@sothx.com>
2025-03-23 03:17:08 +0800

    feat: update

--- src/utils/validateFun.ts
+++ src/utils/validateFun.ts
@@ -90,7 +90,7 @@ export const validateAutoUIRule = (activityRule: string): boolean => {
 
 export const validateAutoUISkipActivityRule = (activityRule: string): boolean => {
     // 正则表达式校验应用布局优化规则
-    const autoUISkipActivityRuleRegex = /^[a-zA-Z0-9_,.]+$/;
+    const autoUISkipActivityRuleRegex = /^[a-zA-Z0-9_;.]+$/;
 
     // 允许空字符串
     if (activityRule === '') {
