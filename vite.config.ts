cebb8157db3fbd3f3a85399add291f63fdae62bd
sothx <i@sothx.com>
2025-07-12 17:58:10 +0800

    Revert "chore: 更新 vite 相关依赖"
    This reverts commit 16be567f9258e17dc8d66e3396cdd1aae01b03f9.

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,25 @@ import { parse } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { exec } from 'child_process';
-import legacy from '@vitejs/plugin-legacy';
+import tailwindcss from 'tailwindcss';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import postcssPresetEnv from 'postcss-preset-env';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 export default defineConfig({
+	css: {
+		postcss: {
+			plugins: [
+				tailwindcss(),
+				postcssPresetEnv({
+					stage: 4,
+				}),
+			],
+		},
+	},
 	plugins: [
 		vue(),
 		vueJsx(),
-		legacy(),
 		vueDevTools(),
 		{
 			name: 'run-powershell-command',
